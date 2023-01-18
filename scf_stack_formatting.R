98fdfccaa4346160985be941041f1cce83efe5af
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-01-18 13:03:56 -0800

    format script

--- scf_stack_formatting.R
+++ scf_stack_formatting.R
@@ -9,8 +9,10 @@ library(spatialEco)
 library(EnvStats)
 library(Kendall)
 
-# format stack
+# set working dir
 setwd("/Users/jacktarricone/ch1_margulis/")
+
+### format stack
 # rast_list <-list.files("./snow_metric_rasters/terra_rasters/scf/", pattern = ".tif", full.names = TRUE)
 # scf_stack <-rast(rast_list)
 # values(scf_stack)[values(scf_stack) == 0] = NA
@@ -18,7 +20,7 @@ setwd("/Users/jacktarricone/ch1_margulis/")
 
 # read in stack 
 scf_stack <-rast("./snow_metric_rasters/terra_rasters/scf/scf_stack.tif")
-scf_
+scf_stack
 
 # test mk code by first running it on .5 degrees lat near tahoe
 american_v1 <-vect("./american_test/american.shp")
@@ -26,6 +28,14 @@ american <-project(american_v1, crs(scf_stack))
 
 # test plot
 plot(scf_stack[[6]])
+plot(american, add = TRUE)
+
+# crop and mask stack down to just american
+scf_american_v1 <-crop(scf_stack, american)
+scf_american <-mask(scf_american_v1, american)
+plot(scf_american[[10]])
+plot(american, add = TRUE)
+
 
 ###### mk test, trend.slope is full stats, 2 is just p-val and slope stats
 trend.slope <- function(y, p.value.pass = TRUE, z.pass = TRUE, 
