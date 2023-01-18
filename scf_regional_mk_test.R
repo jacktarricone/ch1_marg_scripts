b7ce5322eb9f02251ee24d29ccc2e2df78387c73
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-01-18 15:02:32 -0800

    data formatting

--- scf_regional_mk_test.R
+++ scf_regional_mk_test.R
@@ -3,22 +3,15 @@
 # january 18 2023
 
 library(terra)
+library(tidyverse)
 library(rkt)
-
-library(spatialEco)
-library(EnvStats)
-library(Kendall)
 
 # format stack
 setwd("/Users/jacktarricone/ch1_margulis/")
-# rast_list <-list.files("./snow_metric_rasters/terra_rasters/scf/", pattern = ".tif", full.names = TRUE)
-# scf_stack <-rast(rast_list)
-# values(scf_stack)[values(scf_stack) == 0] = NA
-# writeRaster(scf_stack, "./snow_metric_rasters/terra_rasters/scf/scf_stack.tif")
 
-# read in stack 
-scf_stack <-rast("./snow_metric_rasters/terra_rasters/scf/scf_stack.tif")
-scf_
+# read in stack american stack
+scf_stack <-rast("./snow_metric_rasters/terra_rasters/scf/scf_american.tif")
+scf_stack
 
 # test mk code by first running it on .5 degrees lat near tahoe
 american_v1 <-vect("./american_test/american.shp")
@@ -26,6 +19,105 @@ american <-project(american_v1, crs(scf_stack))
 
 # test plot
 plot(scf_stack[[6]])
+
+# bring in ascpet
+aspect_v1 <-rast("/Users/jacktarricone/ch1_margulis/static/rasters/SNSR_aspect.tif")
+aspect <-project(aspect_v1, crs(scf_stack))
+plot(aspect)
+hist(aspect, breaks = 100)
+
+# bin into 4 categories north, south, east, west
+aspect_classes <-matrix(c(315,360,1, 00,45,1, # 1 = north
+                          135,225,2,          # 2 = south
+                          45,135,3,           # 3 = east 
+                          225,315,4),         # 4 = west
+                          ncol=3, byrow=TRUE)
+
+aspect_classes
+
+aspect_cat <-classify(aspect, rcl = aspect_classes)
+plot(aspect_cat)
+hist(aspect_cat)
+# writeRaster(aspect_cat, "./static/aspect_cat.tif")
+
+# mask for american for plotting
+aspect_cat_american_v1 <-crop(aspect_cat, american)
+aspect_cat_american <-mask(aspect_cat_american_v1, american)
+plot(aspect_cat_american)
+hist(aspect_cat_american)
+# writeRaster(aspect_cat_american)
+
+# mask scf with aspect
+scf_stack_mask <-mask(scf_stack, aspect_cat_american)
+
+##### calculate land area of cells that aren't NA
+# aspect_cat
+area_by_direction <-expanse(aspect_cat_american, unit="km", byValue = TRUE)
+aspect_total_area <-expanse(aspect_cat_american, unit="km", byValue = FALSE)
+
+# scf
+scf_total_area <-expanse(scf_stack_mask[[9]], unit="km", byValue = FALSE)
+
+# convert to df
+scf_df <-as.data.frame(scf_stack, xy = TRUE, cells = TRUE, na.rm = TRUE)
+ac_df <-as.data.frame(aspect_cat_american, xy = TRUE, cells = TRUE, na.rm = TRUE)
+ac_df
+
+# filter down to same cell numbers
+scf_filt <-subset(scf_df, cell %in% ac_df$cell)
+ac_filt <-subset(ac_df, cell %in% scf_df$cell)
+
+# years seq
+years <-seq(1985,2016,1)
+
+colnames(scf_filt)[4:ncol(scf_filt)] <-years
+head(scf_filt)
+
+test <-pivot_longer(scf_filt[4:35,], names_to = "year", values_to = "scf")
+
+# check if the same
+identical(scf_filt$cell, ac_filt$cell)
+
+
+data("pie1w")
+pie1w
+rkt(pie1w$Date,pie1w$SO4)
+
+ggplot(pie1w) +
+  geom_line(aes(x = "Date", y = "S04"))
+
+plot(pie1w$Date, pie1w$NO3)
+
+hist(pie1w$Date)
+
+
+##### calculate land area of cells that aren't NA
+# aspect_cat
+area_by_direction <-expanse(aspect_cat_american, unit="km", byValue = TRUE)
+total_area <-expanse(aspect_cat_american, unit="km", byValue = FALSE)
+
+# scf
+scf_total_area <-expanse(scf_stack[[6]], unit="km", byValue = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ###### mk test, trend.slope is full stats, 2 is just p-val and slope stats
 trend.slope <- function(y, p.value.pass = TRUE, z.pass = TRUE, 
