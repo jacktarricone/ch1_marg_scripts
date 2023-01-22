f9db94d229dc3542e337b143829f4b6e519a480d
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-01-22 15:43:02 -0800

    starting avgscript

---
+++ create_avg_metrics.R
@@ -0,0 +1,58 @@
+### create average snow metric rasters
+# january 22nd 2022
+# jack tarricone
+
+library(terra)
+
+#set working directory
+setwd("/Users/jacktarricone/ch1_margulis/snow_metric_rasters/terra_rasters/")
+
+##### compute metriccs
+
+##############
+##### mwa ####
+##############
+
+mwa_list <-list.files('./mwa', pattern = '.tif', full.names = TRUE)
+mwa <-rast(mwa_list)
+
+# mean
+mwa_mean <-app(mwa, fun = "mean", cores=5)
+plot(mwa_mean)
+writeRaster(mwa_mean, "./averages/mwa_mean.tif")
+
+# median
+mwa_med <-app(mwa, fun = 'median', cores=5)
+plot(mwa_med)
+writeRaster(mwa_med, "./averages/mwa_med.tif")
+
+# compare
+global(mwa_mean, 'mean', na.rm = TRUE)
+global(mwa_med, 'mean', na.rm = TRUE)
+hist(mwa_mean, breaks = 100)
+hist(mwa_med, breaks = 100)
+
+##############
+##### max ####
+##############
+
+max_list <-list.files('./max_swe/years', pattern = '.tif', full.names = TRUE)
+max <-rast(max_list)
+
+# mean
+max_mean <-app(max, fun = "mean", cores=5)
+plot(max_mean)
+writeRaster(max_mean, "./averages/max_mean.tif")
+
+# median
+max_med <-app(max, fun = 'median', cores=5)
+plot(max_med)
+writeRaster(max_med, "./averages/max_med.tif")
+
+# compare
+global(max_mean, 'mean', na.rm = TRUE)
+global(max_med, 'mean', na.rm = TRUE)
+hist(max_mean, breaks = 100)
+hist(max_med, breaks = 100)
+
+
