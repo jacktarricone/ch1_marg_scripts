3eaca37947872c3b98cd1b0cf4f384fd0483b1e1
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-03-13 21:15:36 -0700

    formatting

--- plot_average_metric_rasters.R
+++ plot_average_metric_rasters.R
@@ -3,12 +3,19 @@
 
 library(terra)
 library(RColorBrewer)
+library(sf)
 
 #set working directory
 setwd("~/ch1_margulis")
 
-# bring in SNSR shape file
+## bring in SNSR shape file
+# with terra
 snsr <-vect("./static/polygon/SNSR_shp_fixed.shp")
+
+# with sf
+snsr_v1 <-st_read("./static/polygon/SNSR_shp_fixed.shp")
+snsr_sf <-st_geometry(snsr_v1)
+plot(snsr_sf, axes = TRUE)
 
 ##############
 ##### mwa ####
@@ -34,9 +41,25 @@ display.brewer.all()
 scale <-brewer.pal(9, 'YlOrRd')
 
 
-ggplot(mwa_df) +
-  geom_tile(mapping = aes(x,y, fill = mean)) +
-  scale_fill_gradientn(colors = scale, limits = c(0,200), na.value="transparent") +
-  labs(title = "MWA", fill = "MWA (mm)", x = "Lon (deg)", y = "Lat (deg)") +
-  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1)) 
+p1 <-ggplot(mwa_df) +
+       geom_tile(mapping = aes(x,y, fill = mean)) +
+       geom_sf(data = snsr_sf, fill = NA, color = "black", linewidth = .15, inherit.aes = FALSE) + # inherit.aes makes this work
+       scale_fill_gradientn(colors = scale, limits = c(0,200), na.value="transparent",
+                            guide_colorbar(title = "Z Values", 
+                                           title.position = "top", 
+                                           direction = "horizontal", 
+                                           barwidth = 10, 
+                                           barheight = 1, 
+                                           label.position = "bottom",
+                                           label.theme = element_text(size = 12)))
+       labs(fill = "MWA (mm)", x = "Lon (deg)", y = "Lat (deg)") +
+       theme(panel.border = element_rect(colour = "black", fill=NA, linewidth = 1))
+       
+print(p1)
+
+ggsave(p1,
+       file = "./plots/mwa_test_v1.png",
+       width = 7, 
+       height = 9,
+       dpi = 300)
   
