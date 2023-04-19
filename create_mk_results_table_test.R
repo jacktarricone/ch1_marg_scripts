f742b69b12ccf58147a8097f30bee202a04825e3
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-04-19 12:56:05 -0700

    0 = na

--- create_mk_results_table_test.R
+++ create_mk_results_table_test.R
@@ -8,15 +8,17 @@ library(terra)
 setwd("~/ch1_margulis/")
 
 # bring in max mk raster
-max_trend <-rast("./rasters/mk_results/max_slope_full.tif")
+max_trend_v1 <-rast("./rasters/mk_results/max_slope_full.tif")
+max_trend <-subst(max_trend_v1, 0, NA)
 plot(max_trend)
 
+# bring in sig trends
 max_sig_trend_v1 <-rast("./rasters/mk_results/max_sig_slope.tif")
 max_sig_trend  <-subst(max_sig_trend_v1, 0, NA)
 plot(max_sig_trend)
 
 # list shape files
-snsr_basin_paths <-list.files("./vectors/ca_basins/", pattern = "\\.gpkg$", full.names = TRUE)
+snsr_basin_paths <-list.files("./vectors/ca_basins", pattern = "\\.gpkg$", full.names = TRUE)
 snsr_shp_list <-lapply(snsr_basin_paths, vect)
 
 # rough format names
@@ -24,35 +26,45 @@ names_raw <-print(basename(snsr_basin_paths))
 names <-gsub(".gpkg","",names_raw)
 names
 
-basin_mask <-crop(mask(max_trend, snsr_shp_list[[18]]),snsr_shp_list[[18]])
-plot(basin_mask)
-hist(basin_mask, breaks = 100)
+x <-13
+full_trend <-max_trend
+sig_trend <-max_sig_trend
 
-headers <-c("Basin", "Mean Max SWE Trend (mm/yr)", "Max SWE Trend SD (mm/yr)", 
+create_max_mk_table <-function(full_trend, sig_trend, names){
+
+    headers <-c("Basin", "Mean Max SWE Trend (mm/yr)", "Max SWE Trend SD (mm/yr)", 
             "Negative Trends (%)", "Sig. Trends (% of area)")
 
-# calc trend mean
-basin_trend_mean <-round(as.numeric(global(basin_mask, mean, na.rm = TRUE)),2)
+    ### crop and mask full shape for basin
+    basin_trend <-crop(mask(full_trend, snsr_shp_list[[x]]),snsr_shp_list[[x]])
+    plot(basin_trend)
+    basin_sig_trend <-crop(mask(sig_trend , snsr_shp_list[[x]]),snsr_shp_list[[x]])
+    plot(basin_sig_trend)
 
-# calc trend sd
-basin_trend_sd <-round(as.numeric(global(basin_mask, sd, na.rm = TRUE)),2)
+    ### calc trend mean
+    basin_trend_mean <-round(as.numeric(global(basin_trend, 
+                                               mean, na.rm = TRUE)),2)
 
-x <-basin_mask
+    ### calc trend sd
+    basin_trend_sd <-round(as.numeric(global(basin_trend, 
+                                             sd, na.rm = TRUE)),2)
 
-# calc percent of negative trends
-percent_neg_trend <-function(x){
-  total_pixels <-as.numeric(global(x, fun="notNA")) # count pixels not na
-  neg_pixels <-as.numeric(global(x, function(x) sum(x < 0, na.rm=TRUE))) # count pixels with negative trend
-  perc_neg <-round((neg_pixels/total_pixels)*100,1) # calculate percentage
-  return(perc_neg)
+    ### calc percent of negative trends
+    total_pixels <-as.numeric(global(basin_trend, 
+                                     fun="notNA"), na.rm = TRUE) # count pixels not na
+
+    neg_pixels <-as.numeric(global(basin_mask < 0, 
+                                   sum, na.rm=TRUE)) # count pixels with negative trend, https://stackoverflow.com/questions/67442419/number-of-cells-of-raster-under-a-condition
+
+    percent_neg_trend <-round((neg_pixels/total_pixels)*100,1) # calculate percentage
+
+    ### calc percent of negative trends
+    sig_pixels <-as.numeric(global(basin_sig_trend, fun="notNA")) # count sig pixels
+    percent_sig_trend <-round((sig_pixels/total_pixels)*100,1) # calculate percentage
+
+    vals <-c(names[5],)
+
 }
-
-global(basin_mask, function(x) sum(x < 0, na.rm=TRUE))
-
-
-headers <-c("Basin", "Mean Max SWE Trend (mm/yr)", "Max SWE Trend SD (mm/yr)", 
-            "Negative Trends (%)", "Sig. Trends (% of area)")
-
 
 ## bring in SNSR shape file
 # with terra
