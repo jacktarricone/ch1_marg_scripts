4c85f79f093a7c8bdd50a799f102b41840d9e64e
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-04-19 13:19:50 -0700

    working on mk table function

--- create_mk_results_table_test.R
+++ create_mk_results_table_test.R
@@ -30,16 +30,19 @@ x <-13
 full_trend <-max_trend
 sig_trend <-max_sig_trend
 
-create_max_mk_table <-function(full_trend, sig_trend, names){
+create_max_mk_table <-function(x, full_trend, sig_trend, names){
 
-    headers <-c("Basin", "Mean Max SWE Trend (mm/yr)", "Max SWE Trend SD (mm/yr)", 
+    header <-c("Basin", "Mean Max SWE Trend (mm/yr)", "Max SWE Trend SD (mm/yr)", 
             "Negative Trends (%)", "Sig. Trends (% of area)")
 
+    shp_file <-vect(x)
     ### crop and mask full shape for basin
-    basin_trend <-crop(mask(full_trend, snsr_shp_list[[x]]),snsr_shp_list[[x]])
-    plot(basin_trend)
-    basin_sig_trend <-crop(mask(sig_trend , snsr_shp_list[[x]]),snsr_shp_list[[x]])
-    plot(basin_sig_trend)
+    basin_trend <-crop(mask(full_trend, shp_file),shp_file)
+    #plot(basin_trend)
+    # plot(snsr_shp_list[[x]], add = TRUE)
+    basin_sig_trend <-crop(mask(sig_trend , shp_file),shp_file)
+    # plot(basin_sig_trend)
+    # plot(snsr_shp_list[[x]], add = TRUE)
 
     ### calc trend mean
     basin_trend_mean <-round(as.numeric(global(basin_trend, 
@@ -53,7 +56,7 @@ create_max_mk_table <-function(full_trend, sig_trend, names){
     total_pixels <-as.numeric(global(basin_trend, 
                                      fun="notNA"), na.rm = TRUE) # count pixels not na
 
-    neg_pixels <-as.numeric(global(basin_mask < 0, 
+    neg_pixels <-as.numeric(global(basin_trend < 0, 
                                    sum, na.rm=TRUE)) # count pixels with negative trend, https://stackoverflow.com/questions/67442419/number-of-cells-of-raster-under-a-condition
 
     percent_neg_trend <-round((neg_pixels/total_pixels)*100,1) # calculate percentage
@@ -61,10 +64,27 @@ create_max_mk_table <-function(full_trend, sig_trend, names){
     ### calc percent of negative trends
     sig_pixels <-as.numeric(global(basin_sig_trend, fun="notNA")) # count sig pixels
     percent_sig_trend <-round((sig_pixels/total_pixels)*100,1) # calculate percentage
-
-    vals <-c(names[5],)
-
+    
+    # rough format names
+    name_raw <-print(basename(x))
+    name <-gsub(".gpkg","",name_raw)
+    # name
+    
+    vals <-c(name,basin_trend_mean,basin_trend_sd, percent_neg_trend, percent_sig_trend)
+   
+     # vals
+    vals_w_header_v1 <-as.data.frame(rbind(header,vals), row.names = FALSE)
+    vals_w_header <-janitor::row_to_names(vals_w_header_v1, row_number = 1)
+    vals_w_header
+    return(vals_w_header)
 }
+
+# test function
+mk_stats_list_basin <-lapply(snsr_basin_paths, function(x) create_max_mk_table(x,
+                                                                               full_trend = max_trend, 
+                                                                               sig_trend = max_sig_trend, 
+                                                                               names = names))
+mk_stats_list_basin[[2]]
 
 ## bring in SNSR shape file
 # with terra
