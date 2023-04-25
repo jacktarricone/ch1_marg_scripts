7e4affedb005980a8c5c4e7a95470ea17739632a
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-04-25 13:56:41 -0700

    dem classes

--- classify_dem_aspect.R
+++ classify_dem_aspect.R
@@ -10,6 +10,8 @@ setwd("~/ch1_margulis")
 dem <-rast("./rasters/static/SNSR_DEM.tif")
 plot(dem)
 hist(dem, breaks = 100)
+
+# pull out dem max
 dem_max <-as.numeric(global(dem,max,na.rm=T))
 
 # bin into 11 elevation categories by 250m
@@ -24,7 +26,6 @@ plot(dem_3z)
 hist(dem_3z)
 writeRaster(dem_3z, "./rasters/categorized/dem_3z.tif")
 
-
 # compute metricsf
 aspect <-terrain(dem, v = "aspect", neighbors = 8, unit = "degrees")
 slope <-terrain(dem, v = "slope", neighbors = 8, unit = "degrees")
@@ -32,15 +33,15 @@ slope <-terrain(dem, v = "slope", neighbors = 8, unit = "degrees")
 # thres hold aspect to nan under two degrees
 slope_thres <-slope
 values(slope_thres)[values(slope_thres) < 4] = NA
-writeRaster(slope_thres, "./rasters/categorized/slope_4_na.tif", overwrite = TRUE)
+# writeRaster(slope_thres, "./rasters/categorized/slope_4_na.tif", overwrite = TRUE)
 
 # 
 aspect_thres <-mask(aspect, slope_thres, maskvalue = NA)
 plot(aspect_thres)
-writeRaster(aspect_thres, "./rasters/categorized/aspect_thres_4.tif", overwrite = TRUE)
+# writeRaster(aspect_thres, "./rasters/categorized/aspect_thres_4.tif", overwrite = TRUE)
 
-# bin into 11 elevation categories by 250m
-aspect_classes <-matrix(c(0,45,1, # 1 = north
+# bin into 45 degree segments
+aspect_classes_8 <-matrix(c(0,45,1, # 1 = north
                           45,90,2,
                           90,135,3, # 2 = south
                           135,180,4,
@@ -49,13 +50,13 @@ aspect_classes <-matrix(c(0,45,1, # 1 = north
                           270,316,7,
                           315,360,8), # 3 = east 
                           ncol=3, byrow=TRUE)
-aspect_classes
+aspect_classes_8
 
 # classify using matrix
-aspect_cat <-classify(aspect_thres, rcl = aspect_classes)
+aspect_cat <-classify(aspect_thres, rcl = aspect_classes_8)
 plot(aspect_cat)
 hist(aspect_cat)
-writeRaster(aspect_cat, "./rasters/categorized/aspect_thres_8_classes.tif", overwrite = TRUE)
+# writeRaster(aspect_cat, "./rasters/categorized/aspect_thres_8_classes.tif", overwrite = TRUE)
 
 # bin into 11 elevation categories by 250m
 aspect_classes_4 <-matrix(c(0,45,1, # 1  north
@@ -70,76 +71,66 @@ aspect_classes_4
 aspect_cat_4 <-classify(aspect_thres, rcl = aspect_classes_4)
 plot(aspect_cat_4)
 hist(aspect_cat_4)
-writeRaster(aspect_cat_4, "./rasters/categorized/aspect_thres_4_classes.tif", overwrite = TRUE)
-
-
-
-
-
-
-
-
-
-
-
-# bring in aspect ns
-aspect_ns <-rast("./static/aspect_cat_ns.tif")
-aspect_ns_am_v1 <-crop(aspect_ns, american)
-aspect_ns_am <-mask(aspect_ns_am_v1, american)
-# writeRaster(aspect_ns_am, "./static/aspect_ns_am.tif")
+# writeRaster(aspect_cat_4, "./rasters/categorized/aspect_thres_4_classes.tif", overwrite = TRUE)
 
 # test plot
-plot(aspect_ns_am)
-plot(dem_am_cat)
+plot(aspect_cat_4)
+plot(dem_3z)
 
 ##### add north and south classification
-# b1_n = 1
-test <-dem_am_cat == 1 &  # b1
-       aspect_ns_am == 1  # n
-test <-subst(test,TRUE,1)
-test <-subst(test,0,NA)
-plot(test, col = 'red')
+#### ez1_n = 1
+ez1_n <-dem_3z == 1 &  # ez1
+       aspect_cat_4 == 1  # north
+ez1_n <-subst(ez1_n,TRUE,1)
+ez1_n <-subst(ez1_n,0,NA)
+plot(ez1_n, col = 'red')
+# writeRaster(ez1_n, "./rasters/categorized/ez1_n.tif")
 
-# b1_s = 2
-test_v2 <-dem_am_cat == 1 & # b1
-          aspect_ns_am == 2 # s
-test_v2 <-subst(test_v2,TRUE,2)
-test_v2 <-subst(test_v2,0,NA)
-plot(test_v2, col = 'darkgreen', add = TRUE)
+##### ez1_s = 2
+ez1_s <-dem_3z == 1 & # ez1
+          aspect_cat_4 == 3 # south
+ez1_s <-subst(ez1_s,TRUE,2)
+ez1_s <-subst(ez1_s,0,NA)
+plot(ez1_s, col = 'darkgreen', add = TRUE)
+# writeRaster(ez1_s, "./rasters/categorized/ez1_s.tif")
 
-# b2_n = 3
-test_v3 <-dem_am_cat == 2 &  # b2
-          aspect_ns_am == 1  # n
-test_v3 <-subst(test_v3,TRUE,3)
-test_v3 <-subst(test_v3,0,NA)
-plot(test_v3, col = 'purple', add = TRUE)
+##### ez2_n = 3
+ez2_n <-dem_3z == 2 &  # ez2
+          aspect_cat_4 == 1  # north
+ez2_n <-subst(ez2_n,TRUE,3)
+ez2_n <-subst(ez2_n,0,NA)
+plot(ez2_n, col = 'purple', add = TRUE)
+# writeRaster(ez2_n, "./rasters/categorized/ez2_n.tif")
 
-# b2_s = 4
-test_v4 <-dem_am_cat == 2 &  # b2
-          aspect_ns_am == 2  # s
-test_v4 <-subst(test_v4,TRUE,4)
-test_v4 <-subst(test_v4,0,NA)
-plot(test_v4, col = 'darkorange', add = TRUE)
+##### ez2_s = 3
+ez2_s <-dem_3z == 2 &  # ez2
+        aspect_cat_4 == 3  # south
+ez2_s <-subst(ez2_s,TRUE,4)
+ez2_s <-subst(ez2_s,0,NA)
+plot(ez2_s, col = 'firebrick', add = TRUE) 
+# writeRaster(ez2_s, "./rasters/categorized/ez2_s.tif")
 
-# b3_n = 5
-test_v5 <-dem_am_cat == 3 &  # b3
-          aspect_ns_am == 1  # n
-test_v5 <-subst(test_v5,TRUE,5)
-test_v5 <-subst(test_v5,0,NA)
-plot(test_v5, col = 'grey50', add = TRUE)
+##### ez3_n = 5
+ez3_n <-dem_3z == 3 &  # ez3
+          aspect_cat_4 == 1  # north
+ez3_n <-subst(ez3_n,TRUE,5)
+ez3_n <-subst(ez3_n,0,NA)
+plot(ez3_n, col = 'grey50', add = TRUE)
+# writeRaster(ez3_n, "./rasters/categorized/ez3_n.tif")
 
-# b3_s = 6
-test_v6 <-dem_am_cat == 3 &  # b3
-          aspect_ns_am == 2  # s
-test_v6 <-subst(test_v6,TRUE,6)
-test_v6 <-subst(test_v6,0,NA)
-plot(test_v6, col = 'yellow', add = TRUE)
+##### ez3_s = 5
+ez3_s <-dem_3z == 3 &  # ez3
+  aspect_cat_4 == 3  # south
+ez3_s <-subst(ez3_s,TRUE,6)
+ez3_s <-subst(ez3_s,0,NA)
+plot(ez3_s, col = 'yellow', add = TRUE)
+# writeRaster(ez3_s, "./rasters/categorized/ez3_s.tif")
 
 # mosaic back together
-stitch <-mosaic(test,test_v2,test_v3,test_v4,test_v5,test_v6)
+stitch <-mosaic(ez1_n, ez1_s, ez2_n, ez2_s, ez3_n, ez3_s)
 plot(stitch)
 
-writeRaster(stitch, "./static/american_dem_aspect_classified.tif")
+writeRaster(stitch, "./rasters/categorized/dem_ez3_ns.tif")
 
 
 # calculate percentage of land area
