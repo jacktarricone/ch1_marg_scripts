1e87db337269a6120a4c203fcdb928e4aafe1470
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-05-10 15:32:59 -0700

    priz

--- prism_download.R
+++ prism_download.R
@@ -11,17 +11,31 @@ setwd("~/ch1_margulis")
 prism_set_dl_dir("./rasters/prism")
 
 # download normals
-get_prism_normals(type="tmean", resolution = "800m", mon = 1:6, keepZip = FALSE)
+get_prism_normals(type="tmean", resolution = "800m", mon = 1:12, keepZip = FALSE)
+get_prism_normals(type="tmax", resolution = "800m", mon = 1:12, keepZip = FALSE)
+get_prism_normals(type="tmean", resolution = "800m", mon = 1:12, keepZip = FALSE)
 pd_get_name(prism_archive_ls())
 
-jmean <- prism_archive_subset(
+jan_mean <- prism_archive_subset(
   "tmean", "monthly normals", mon = 1, resolution = "800m"
 )
-file <-pd_to_file(jmean)
-jmean_rast <-rast(file)
-plot(jmean_rast)
 
+
+file <-pd_to_file(jmean)
+rast <-rast(file)
+plot(rast)
+
+# load in dem for formatting
 dem <-rast("./rasters/static/SNSR_DEM.tif")
 
-jmean_rast_c <-crop(jmean_rast, ext(dem))
-plot(jmean_rast_c)
+# reproject and crop
+rast_v1 <-project(rast, crs(dem))
+rast_v2 <-crop(rast_v1, ext(dem))
+plot(rast_v2)
+
+# resample and mask
+rast_v3 <-resample(rast_v2, dem, method = 'bilinear')
+rast_v4 <-mask(rast_v3, dem, maskvalue = NA)
+plot(rast_v4)
+writeRaster(rast_v4, "./rasters/prism/prism_tmean_test.tif")
+hist(rast_v4, breaks = 100)
