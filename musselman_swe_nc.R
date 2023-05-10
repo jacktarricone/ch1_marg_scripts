b64cabcf58f463307cd218ff3eec2eef5a04f841
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-05-10 14:22:02 -0700

    starting prism

--- musselman_swe_nc.R
+++ musselman_swe_nc.R
@@ -6,7 +6,12 @@ muss_test
 
 names <-ncvar_get(muss_test, "agency")
 siteID <-ncvar_get(muss_test, "siteID")
-swe_array<-ncvar_get(muss_test, "swe_level2")
+swe_array <-ncvar_get(muss_test, "swe_level2")
+lat_array <-ncvar_get(muss_test, "latitude")
+lon_array <-ncvar_get(muss_test, "longitude")
+ele_array <-ncvar_get(muss_test, "elevation")
+date_matlab <-ncvar_get(muss_test, "date_matlab")
+
 head(swe_array)
 dim(swe_array)
 plot(swe_array[,1,52])
