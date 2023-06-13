7729d812a5afa63a422008119ccbc78977754b03
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-06-12 17:06:43 -0700

    heatmapping

--- make_spearman_table.R
+++ make_spearman_table.R
@@ -4,54 +4,136 @@
 
 library(tidyverse)
 library(data.table)
+library(cowplot)
 
 # set wd
 setwd("~/ch1_margulis")
 
-# # read in csvs from each basin
-# csv_list <-list.files("./csvs/spearman_fm_temp_results", full.names = TRUE)
-# df_list <-lapply(csv_list[2:21], fread)
-# big_df <-bind_rows(df_list)
-# 
-# # format aspect and elevation zones
-# # add ez
-# big_df$zone_name <-ifelse(big_df$ez == 1, "1500-1900 m", big_df$ez)
-# big_df$zone_name <-ifelse(big_df$ez == 2, "1900-2300 m", big_df$zone_name)
-# big_df$zone_name <-ifelse(big_df$ez == 3, "2300-2700 m", big_df$zone_name)
-# big_df$zone_name <-ifelse(big_df$ez == 4, "2700-3100 m", big_df$zone_name)
-# big_df$zone_name <-ifelse(big_df$ez == 5, "3100-3500 m", big_df$zone_name)
-# big_df$zone_name <-ifelse(big_df$ez == 6, "3500-4361 m", big_df$zone_name)
-# 
-# # aspect name
-# big_df$aspect_name <-ifelse(big_df$aspect == 1, "North", big_df$aspect)
-# big_df$aspect_name <-ifelse(big_df$aspect == 2, "East",  big_df$aspect_name)
-# big_df$aspect_name <-ifelse(big_df$aspect == 3, "South", big_df$aspect_name)
-# big_df$aspect_name <-ifelse(big_df$aspect == 4, "West", big_df$aspect_name)
-# 
-# # capitalize names
-# # aspect name
-# big_df$basin_name <-stringr::str_to_title(big_df$basin_name)
-# big_df$basin_name <-ifelse(big_df$basin_name == "Upper_sac",  "Upper Sacramento", big_df$basin_name)
-# big_df$basin_name <-ifelse(big_df$basin_name == "Usj",        "Upper San Joaquin", big_df$basin_name)
-# unique(big_df$basin_name)
-# 
-# fwrite(big_df, "./csvs/spearman_fm_temp_results/all_basins_spearman_results.csv", row.names = FALSE)
+# set custom plot theme
+theme_classic <-function(base_size = 11, base_family = "",
+                         base_line_size = base_size / 22,
+                         base_rect_size = base_size / 22) {
+  theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    theme(
+      # no background and no grid
+      panel.border     = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      
+      # show axes
+      # axis.line      = element_line(colour = "black", linewidth = rel(1)),
+      
+      # match legend key to panel.background
+      legend.key       = element_blank(),
+      
+      # simple, black and white strips
+      strip.background = element_rect(fill = "white", colour = "black", linewidth = rel(2)),
+      # NB: size is 1 but clipped, it looks like the 0.5 of the axes
+      
+      complete = TRUE
+    )
+}
 
+theme_set(theme_classic(15))
+
+# read in df
 df <-fread("./csvs/spearman_fm_temp_results/all_basins_spearman_results.csv")
 head(df)
 
-# write function which caluclates pertaage of bin that is significant
+# write function which calculates percentage of bin that is significant
 results_v1 <-df %>%
-  group_by(basin_name, zone_name) %>%
+  group_by(Basin, zone_name, aspect_name) %>%
   summarise(percent_sig     = round((length(which(p_val < .05))/length(p_val))*100, 0))
 
-test <-as.data.frame(results_v1 %>%
-  pivot_wider(names_from = zone_name, values_from = percent_sig))
+head(results_v1)
 
-test
+# filter for north and south facing
+north_results <-filter(results_v1, aspect_name == "North")
+south_results_v1 <-filter(results_v1, aspect_name == "South")
 
-z %>%
-  makeHeatmap(palette="Reds") %>%
-  print(caption="Table 5. Heatmap table with 'Blue' palette")
+# remove zone 4 feather, like 4 pixels
+south_results <-filter(south_results_v1, Basin != "Feather" | zone_name != "2300-2700 m")
+diff <-north_results$percent_sig - south_results$percent_sig
 
+# make differnce df
+diff_results <-cbind(north_results, diff)
 
+# # pivot wider for plotting
+# test <-as.data.frame(results_v1 %>%
+#   pivot_wider(names_from = zone_name, values_from = percent_sig))
+
+# plot
+mycolors <-brewer.pal(9, "YlOrRd")
+
+#### north facing
+north_p <-ggplot(north_results, aes(y=Basin, x=zone_name, fill= percent_sig)) + 
+  geom_tile()+
+  geom_text(aes(label=percent_sig)) +
+  scale_fill_gradientn(colors = mycolors) +
+  labs(x = "EZ", fill = "Percentage Significant (%)", title = "North Facing") +
+  scale_x_discrete(expand = c(0, 0))+
+  scale_y_discrete(expand = c(0, 0))+
+  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth =1),
+        panel.background = element_rect(fill = 'gray'),
+        aspect.ratio = 1,
+        legend.position  = 'bottom',
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        plot.margin = unit(c(.25,.1,.1,.1), "cm"),
+        legend.box.spacing = unit(0, "pt")) +
+  guides(fill = guide_colorbar(direction = "horizontal",
+                                label.position = 'top',
+                                title.position = 'bottom',
+                                title.hjust = .5,
+                                barwidth = 27,
+                                barheight = 1,
+                                frame.colour = "black", 
+                                ticks.colour = "black"))
+north_p
+
+ggsave(north_p,
+       file = "./plots/spearman_heat_north_v1.png",
+       width = 8, 
+       height = 8,
+       dpi = 600)
+
+system("open ./plots/spearman_heat_north_v1.png")
+
+#### south facing
+south_p <-ggplot(south_results, aes(y=Basin, x=zone_name, fill= percent_sig)) + 
+  geom_tile()+
+  geom_text(aes(label=percent_sig)) +
+  scale_fill_gradientn(colors = mycolors) +
+  labs(x = "EZ", fill = "Percentage Significant (%)", title = "South Facing") +
+  scale_x_discrete(expand = c(0, 0))+
+  scale_y_discrete(expand = c(0, 0))+
+  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth =1),
+        panel.background = element_rect(fill = 'gray'),
+        aspect.ratio = 1,
+        legend.position  = 'bottom',
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        plot.margin = unit(c(.25,.1,.1,.1), "cm"),
+        legend.box.spacing = unit(0, "pt")) +
+  guides(fill = guide_colorbar(direction = "horizontal",
+                               label.position = 'top',
+                               title.position = 'bottom',
+                               title.hjust = .5,
+                               barwidth = 27,
+                               barheight = 1,
+                               frame.colour = "black", 
+                               ticks.colour = "black"))
+south_p
+
+ggsave(south_p,
+       file = "./plots/spearman_heat_south_v1.png",
+       width = 8, 
+       height = 8,
+       dpi = 600)
+
+system("open ./plots/spearman_heat_south_v1.png")
