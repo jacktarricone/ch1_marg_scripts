1fba10ff66231b016def689ac913304eeb89c880
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2023-06-27 19:04:47 -0700

    validating

--- snsr_snotel_metric_compare_v2.R
+++ snsr_snotel_metric_compare_v2.R
@@ -110,15 +110,15 @@ max_25 <-ggplot(data = max_df, mapping = aes(x = max_snotel_25.4, y = max_snsr_2
 
 plot(max_25)
 
-# save
-ggsave( "./plots/max_metric_compare_v8.pdf",
-       max_25,
-       width = 4.5,
-       height = 4.5,
-       units = "in")
-
-system("open ./plots/max_metric_compare_v8.pdf")
-
+# # save
+# ggsave( "./plots/max_metric_compare_v8.pdf",
+#        max_25,
+#        width = 4.5,
+#        height = 4.5,
+#        units = "in")
+# 
+# system("open ./plots/max_metric_compare_v8.pdf")
+# 
 
 #########################
 #########################
@@ -151,18 +151,18 @@ max_dowy_25 <-ggplot(data = max_dowy_df, mapping = aes(x = max_dowy_snotel_25.4,
 
 plot(max_dowy_25)
 
-# save
-ggsave( "./plots/max_dowy_metric_compare_v6.pdf",
-       max_dowy_25,
-       width = 4.5,
-       height = 4.5,
-       units = "in")
-
-system("open ./plots/max_dowy_metric_compare_v6.pdf")
+# # save
+# ggsave( "./plots/max_dowy_metric_compare_v6.pdf",
+#        max_dowy_25,
+#        width = 4.5,
+#        height = 4.5,
+#        units = "in")
+# 
+# system("open ./plots/max_dowy_metric_compare_v6.pdf")
 
 ##########################
 ##########################
-##     fm   ##
+#######    fm   ##########
 ##########################
 ##########################
 
@@ -178,7 +178,7 @@ fm_df <-as.data.frame(swe_df %>%
 
 # plot
 fm_25 <-ggplot(fm_df, aes(x = fm_snotel_25.4, y = fm_snsr_25.4)) +
-  geom_pointdensity(adjust = .05, size = 1) +
+  geom_pointdensity(adjust = .1, size = 1) +
   scale_color_viridis(option = "H") +
   geom_abline(intercept = 0, slope = 1, linetype = 2, linewidth = .25) +
   scale_y_continuous(limits = c(0,1),expand = (c(0,0))) +
@@ -190,34 +190,106 @@ fm_25 <-ggplot(fm_df, aes(x = fm_snotel_25.4, y = fm_snsr_25.4)) +
 
 plot(fm_25)
 
-# save
-ggsave( "./plots/fm_metric_compare_v2.pdf",
-        fm_25,
-        width = 4.5,
-        height = 4.5,
-        units = "in")
+# # save
+# ggsave( "./plots/fm_metric_compare_v2.pdf",
+#         fm_25,
+#         width = 4.5,
+#         height = 4.5,
+#         units = "in")
+# 
+# system("open ./plots/fm_metric_compare_v2.pdf")
 
-system("open ./plots/fm_metric_compare_v2.pdf")
+
+##########################
+##########################
+#######    mwa   ##########
+##########################
+##########################
+
+mwa_ondjfm_mm_v1 <-function(x, swe_thres){
+  
+  # set threshold
+  if (max(x) < swe_thres){
+    return(NA)
+  } else {x}
+  if (length(x) == 365){ # non leap year
+    
+    # trim vector to dec 1 - march 31
+    djfm <-x[1:182]
+    
+    # find difference between values
+    val_diff <-diff(djfm)
+    val_diff
+    
+    # sum all negative values
+    mwa_mm <-abs(sum(val_diff[val_diff<0]))
+    mwa_mm
+    return(mwa_mm)
+  }
+  else{ # leap year
+    # trim vector to dec 1 - march 31
+    djfm <-x[1:183]
+    
+    # find difference between values
+    val_diff <-diff(djfm)
+    val_diff
+    
+    # sum all negative values
+    mwa_mm <-abs(sum(val_diff[val_diff<0]))
+    mwa_mm
+    return(mwa_mm)
+  }
+}
+
+# calc metric 
+mwa_df <-as.data.frame(swe_df %>%
+                        group_by(site_name, waterYear) %>%
+                        summarise(mwa_snotel_25.4 = mwa_ondjfm_mm_v1(snotel_swe_mm, swe_thres = 25.4),
+                                  mwa_snsr_25.4   = mwa_ondjfm_mm_v1(snsr_swe_mm, swe_thres = 25.4)))
+head(mwa_df)
+max(mwa_df$mwa_snotel_25.4, na.rm = TRUE)
+
+# plot
+mwa_25 <-ggplot(mwa_df, aes(x = mwa_snotel_25.4, y = mwa_snsr_25.4)) +
+  geom_pointdensity(adjust = 50, size = 1) +
+  scale_color_viridis(option = "H") +
+  geom_abline(intercept = 0, slope = 1, linetype = 2, linewidth = .25) +
+  scale_y_continuous(limits = c(0,600),expand = (c(0,0))) +
+  scale_x_continuous(limits = c(0,600),expand = (c(0,.02))) +
+  xlab("SNOTEL MWA (mm)") + ylab("SNSR MWA (mm)") +
+  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth =1),
+        aspect.ratio = 1,
+        legend.position = "none")
+
+plot(mwa_25)
+
+# # save
+# ggsave( "./plots/mwa_metric_compare_v2.pdf",
+#         mwa_25,
+#         width = 4.5,
+#         height = 4.5,
+#         units = "in")
+# 
+# system("open ./plots/mwa_metric_compare_v2.pdf")
 
 
 
 # stack with cow plot
-plot_grid(max_25,max_dowy_25, fm_25,
-          labels = c("(a)","(b)","(c)"),
+plot_grid(max_25,max_dowy_25, fm_25, mwa_25,
+          labels = c("(a)","(b)","(c)","(d)"),
           align = "hv",
           vjust =  2,
           hjust = -.2,
-          ncol = 3, 
-          rel_widths = c(1/3, 1/3, 1/3))
+          ncol = 2, 
+          rel_widths = c(1/2, 1/2))
 
-ggsave("./plots/snsr_snotel_metric_compare_v11.pdf",
-       width = 13.5, 
-       height = 4.5,
+ggsave("./plots/snsr_snotel_metric_compare_new_v1.pdf",
+       width = 8, 
+       height = 8,
        units = "in",
        dpi = 500)
 
-system("open ./plots/snsr_snotel_metric_compare_v11.pdf")
-
+system("open ./plots/snsr_snotel_metric_compare_new_v1.pdf")
 
 #########################################
 ### calculate goodness of it metrics ####
@@ -237,22 +309,23 @@ gof_func <-function(snsr, snotel, metric_name){
   pb <-round(hydroGOF::pbias(snsr, snotel, na.rm = TRUE), digits = 2)
   
   # bind
-  row <-c(metric_name,corr,r2,rmse,nrmse,mae,nmae,me,pb)
+  row <-c(metric_name,corr,r2,rmse,mae,me,pb)
   return(row)
 }
 
 # create rows
-metric_names <-c("Snow Metric", "R", "R^2", "RMSE","NRMSE","MAE","NMAE","ME", "PB (%)")
+metric_names <-c("Snow Metric", "R", "R^2", "RMSE","MAE","ME", "PB (%)")
 max_stats <-gof_func(max_df$max_snsr_25.4, max_df$max_snotel_25.4, "Max SWE (m)")
 max_dowy_stats <-gof_func(max_dowy_df$max_dowy_snsr_25.4, max_dowy_df$max_dowy_snotel_25.4, "Max SWE (DOWY)")
 fm_stats <-gof_func(fm_df$fm_snsr_25.4, fm_df$fm_snotel_25.4, "FM")
+mwa_stats <-gof_func(mwa_df$mwa_snsr_25.4, mwa_df$mwa_snotel_25.4, "MWA (mm)")
 fm_stats
 
 # make df 
-table <-as.data.frame(rbind(max_stats, max_dowy_stats, fm_stats))
+table <-as.data.frame(rbind(max_stats, max_dowy_stats, fm_stats, mwa_stats))
 
 # rename cols
-colnames(table)[1:9] <-metric_names
+colnames(table)[1:7] <-metric_names
 # table <-table[-1,]
 table
 
